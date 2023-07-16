a80df49d3b7c701e68cd042e9df9fec2f8ce0695
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-07-16 14:21:55 -0400

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const num1= 31
+const num2 = 2
+
+const multiply = num1*num2
+
+const random = Math.floor(Math.random() * 10)
+
+//console.log(randomNum)
+const num3 = 9
+const num4 = 5
+
+const mod = num3 % num4
+
+const max = Math.max(3,4,5,1,13,20)
